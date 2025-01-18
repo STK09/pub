b2e43e2bd5cf1c03331034aeadbcc9ac51752058
STK09 <mkeityrs@gmail.com>
2025-01-18 16:05:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "8014133263:AAEoIqNj0C4ycpaNCPgUwyzCKcLexVEeE5Y"
-MONGO_URI = "your-mongodb-uri"
+MONGO_URI = "mongodb+srv://mkeityrs:mkeityrs@cluster0.czt46.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_CHANNEL = -1002302578351  # Replace with your channel ID for file storage
 LOG_CHANNEL = -1002386303813  # Replace with your log channel ID
 BOT_USERNAME = "Testyhfffgfewsbot"
 
-ADMIN_IDS = [123456789, 987654321]  # List of admin user IDs who can use broadcast
+ADMIN_IDS = [5827289728, 7272399911]  # List of admin user IDs who can use broadcast
