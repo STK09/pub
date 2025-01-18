3927d24cfda5edd7bd6e3944c555b540e6bc691f
STK09 <mkeityrs@gmail.com>
2025-01-18 15:58:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8014133263:AAEoIqNj0C4ycpaNCPgUwyzCKcLexVEeE5Y"
+MONGO_URI = "your-mongodb-uri"
+DATABASE_CHANNEL = -1002302578351  # Replace with your channel ID for file storage
+LOG_CHANNEL = -1002386303813  # Replace with your log channel ID
+BOT_USERNAME = "Testyhfffgfewsbot"
+
+ADMIN_IDS = [123456789, 987654321]  # List of admin user IDs who can use broadcast
